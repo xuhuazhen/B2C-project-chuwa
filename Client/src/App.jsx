c836b0ed3e171c089b82355fc23cc8febb0faa40
xuhuazhen <160662429+xuhuazhen@users.noreply.github.com>
2025-10-10 19:42:48 -0700

    Update App.jsx

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,26 +1,42 @@
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
-import CreateProductPage from './pages/createNewProduct'; 
+import { Flex, Layout } from "antd";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import CreateProductPage from "./pages/createNewProduct";
+import AppHeader from "./components/header";
+import AppFooter from "./components/footer";
+const { Header, Footer, Sider, Content } = Layout;
 import LoginPage from './pages/Login/Login'; 
 import SignupPage from './pages/Signup/Signup';
 import { ChangePwdPage } from './pages/ChangePwd/ChangPwd';
 
 function Home() {
   return (
-    <div style={{ padding: 24 }}>
-      <h1>Home Page</h1>
-      <p><Link to="/admin/create-product">Go to Create Product Page</Link></p>
-      <p><Link to="/login">Go to login Page</Link></p>
-    </div>
+    <Flex gap="middle" wrap>
+      <Layout>
+        <AppHeader />
+        <Content>Content</Content>
+        {/* <Footer>Footer</Footer> */}
+        <AppFooter></AppFooter>
+      </Layout>
+    </Flex>
   );
-  // return (
-  //   <div style={{ padding: 24 }}>
-  //     <h1>Home Page</h1>
-  //     <p>
-  //       <Link to="/admin/create-product">Go to Create Product Page</Link>
-  //     </p>
-  //   </div>
-  // );
-}
+
+// function Home() {
+//   return (
+//     <div style={{ padding: 24 }}>
+//       <h1>Home Page</h1>
+//       <p><Link to="/admin/create-product">Go to Create Product Page</Link></p>
+//       <p><Link to="/login">Go to login Page</Link></p>
+//     </div>
+//   );
+//   // return (
+//   //   <div style={{ padding: 24 }}>
+//   //     <h1>Home Page</h1>
+//   //     <p>
+//   //       <Link to="/admin/create-product">Go to Create Product Page</Link>
+//   //     </p>
+//   //   </div>
+//   // );
+// }
 
 export default function App() {
   return (
