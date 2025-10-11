d3d60c2ec45beaa337bb7ac23623dcc4d7365f48
xuhuazhen <160662429+xuhuazhen@users.noreply.github.com>
2025-10-10 19:46:29 -0700

    Revert "Shuna li"

--- Server/controllers/productController.js
+++ Server/controllers/productController.js
@@ -1,37 +1,13 @@
-import { catchAsync } from "../utils/catchAsync.js";
-import { Product } from "../modles/Product.js";
+import { catchAsync } from '../utils/catchAsync.js';
+import { Product } from '../modles/Product.js';
 
 export const get_products = catchAsync(async (req, res, next) => {
-  const products = await Product.find({ isActive: true }).sort({
-    createdAt: -1,
-  });
-
-  res.status(200).json({
-    status: "success",
-    message: products.length === 0 ? "list is empty" : "",
-    products,
-  });
-});
-
-// GET /api/product/search?q=
-export const get_search = catchAsync(async (req, res, next) => {
-  const query = req.query.q;
-  console.log("Search hit. Query:", query);
-
-  if (!query?.trim()) {
-    console.log("Empty query");
-    return res.json([]);
-  }
-
-  const products = await Product.find(
-    { name: { $regex: query, $options: "i" } },
-    { name: 1 }
-  ).limit(10);
-
-  //   console.log("Found products:", products);
-
-  res.status(200).json({
-    status: "success",
-    products,
-  });
+    const products = await Product.find({ isActive: true })
+        .sort({ createdAt: -1 });
+    
+    res.status(200).json({
+        status: 'success',
+        message: products.length === 0 ? 'list is empty' : '',
+        products
+    })
 });
